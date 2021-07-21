c6be68d80b399864359ad4d19adc4d07a227c88d
Philip Otoo <pluxcon@gmail.com>
2021-07-21 11:22:19 +0000

    payment transactions table add to admin dashboard, momentjs npm package install

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -15,7 +16,8 @@ class DashboardController extends Controller
 
     public function index()
     {
-        $title = 'Admin Dashboard';
-        return view('admin.dashboard')->with(['title' => $title]);
+        $data['title'] = "Admin Dashboard";
+        $data['transactions'] = DB::table('coinpayment_transactions')->get();
+        return view('admin.dashboard', compact('data'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,74 @@
 @extends('admin.layouts.master')
 
 @section('title')
-    {{ $title }}
+    {{ $data['title'] }}
 @endsection
 
 @section('content')
 
 @include('admin.partials.top_widgets')
 
+<div class="row justify-content-center">
+    <div class="col-12">
+        <div class="card card-primary">
+            <div class="card-header">
+                <div class="card-title">Payment transactions</div>
+            </div>
+            <div class="card-body">
+                <table class="table table-striped" id="transactionTbl" style="width: 100%">
+                    <thead>
+                        <tr>
+                            <th>USERNAME</th>
+                            <th>EMAIL</th>
+                            <th>USD</th>
+                            <th>BTC</th>
+                            <th>ADDRESS</th>
+                            <th>STATUS</th>
+                            <th>DATE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data['transactions'] as $transac)
+                        <tr>
+                            <td>{{ $transac->buyer_name }}</td>
+                            <td>{{ $transac->buyer_email }}</td>
+                            <td>{{ $transac->amount_total_fiat }}</td>
+                            <td>{{ $transac->amountf }}</td>
+                            <td>{{ $transac->address }}</td>
+                            <td>
+                                @if($transac->status < -1)
+                                <span class="badge badge-danger">Failed/Cancel</span>
+                                @endif
+
+                                @if($transac->status == 0 && $transac->status <= 0)
+                                <span class="badge badge-info">Pending</span>
+                                @endif
+
+                                @if($transac->status >= 100)
+                                <span class="badge badge-success">Success</span>
+                                @endif
+                            </td>
+                            <td>{{ date('d-M-Y', strtotime($transac->created_at)) }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
 
 
 
 @section('extra_script')
-
+<script>
+    $(function(){
+        $('#transactionTbl').DataTable({
+            scrollX: true,
+            scrollY: true
+        });
+    });
+</script>
 @endsection
